a396362bf5e7fbd4f778ae2fa47ebda8d1e6bfcb
dwd03 <drpeperoni@outlook.com>
2020-12-15 09:38:28 +0100

    awd

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 app = Flask(__name__)
 
 @app.route("/")
-def mainpage(): #STARTPAGE
+def mainpage():
     return render_template("index.html")
 
 @app.route("/mamma")
@@ -12,18 +12,17 @@ def mamma():
 @app.route("/pappa")
 def pappa(): 
     pappaLinks = ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"]
-    return render_template("pappa.html",currentName="Pappa", imageLink="pappa",links=pappaLinks)
+    return render_template("pappa.html",currentName="Pappa", imageLink=url_for('static', filename='pappa.png'),links=pappaLinks)
 
 @app.route("/linnea")
 def linnea(): 
-    return render_template("linnea.html")
+    linneaLinks = []
+    return render_template("linnea.html",currentName="Linnea", imageLink=url_for('static', filename='linnea.png'),links=linneaLinks)
 
 @app.route("/oskar")
 def oskar(): 
-    return render_template("oskar.html")
-
-
-
+    oskarLinks = []
+    return render_template("oskar.html",currentName="Oskar", imageLink=url_for('static', filename='oskar.png'),links=oskarLinks)
 
 if __name__ == "__main__":
     try:
