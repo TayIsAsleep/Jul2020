700ab2ca565a3f664f6954c2b8be856bf14b0b47
dwd03 <drpeperoni@outlook.com>
2020-12-15 11:37:29 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -26,16 +26,16 @@ def oskar():
     oskarLinks = []
     return render_template("index.html", currentName="Oskar", imageLink=url_for('static',filename='oskar.png'), links=oskarLinks, personalMessage=oskarText)
 
-if __name__ == "__main__":
-    try:
-        ipConfig = []
-        with open("private.txt","r+") as f:
-            for x in f.readlines():
-                ipConfig.append(x.strip("\n"))
-        app.run(debug=False, host=ipConfig[0], port=ipConfig[1])
-    except FileNotFoundError:
-        input("ERROR : Could not find IP file, please create a file called 'ip.txt' in the root and put server ip and port")
-    except OSError:
-        input("ERROR : Incorrect IP")
-    else:
-        pass
+# if __name__ == "__main__":
+#     try:
+#         ipConfig = []
+#         with open("private.txt","r+") as f:
+#             for x in f.readlines():
+#                 ipConfig.append(x.strip("\n"))
+#         app.run(debug=False, host=ipConfig[0], port=ipConfig[1])
+#     except FileNotFoundError:
+#         input("ERROR : Could not find IP file, please create a file called 'ip.txt' in the root and put server ip and port")
+#     except OSError:
+#         input("ERROR : Incorrect IP")
+#     else:
+#         pass
