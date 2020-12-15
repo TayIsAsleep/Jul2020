492928df830a17efc7120e81ac8338c5ce1cd5d8
dwd03 <drpeperoni@outlook.com>
2020-12-15 11:28:33 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route("/mamma")
 def mamma(): 
-    mammaText = "God Jul till dig Mamma."
+    mammaText = "God Jul till dig Mamma. Tack för att du står ut med mig varje dag, även om jag kan bli jobbig ibland ❤️"
     mammaLinks = []
     return render_template("index.html", currentName="Mamma", imageLink=url_for('static', filename='mamma.png'), links=mammaLinks, personalMessage=mammaText)
 
 @app.route("/pappa")
 def pappa(): 
-    pappaText = "God Jul till den bästa pappan som finns! Tack för allt du gör för mig, och för allt vi har gjort genom åren."
+    pappaText = "God Jul till den bästa pappan som finns! Tack för allt du gör för mig, och för allt vi har gjort genom åren. ❤️"
     pappaLinks = ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"]
     return render_template("index.html", currentName="Pappa", imageLink=url_for('static',filename='pappa.png'), links=pappaLinks, personalMessage=pappaText)
 
@@ -22,7 +22,7 @@ def linnea():
 
 @app.route("/oskar")
 def oskar(): 
-    oskarText = "Oskar Text"
+    oskarText = "God Jul till Oskar, och tack för att du tar hand om min stora-syster!"
     oskarLinks = []
     return render_template("index.html", currentName="Oskar", imageLink=url_for('static',filename='oskar.png'), links=oskarLinks, personalMessage=oskarText)
 
