acda1dfde1ba686fa91b5a4e9fc44962d0171287
dwd03 <drpeperoni@outlook.com>
2020-12-15 09:26:33 +0100

    test

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for
 app = Flask(__name__)
 
 @app.route("/")
 def mainpage(): #STARTPAGE
     return render_template("index.html")
 
+@app.route("/mamma")
+def mamma(): 
+    return render_template("mamma.html")
+
 @app.route("/pappa")
 def pappa(): 
-    return render_template("pappa.html")
+    pappaLinks = ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"]
+    return render_template("pappa.html",currentName = "Pappa", imageLink = url_for('static', filename='pappa.png') ,links = pappaLinks)
+
+@app.route("/linnea")
+def linnea(): 
+    return render_template("linnea.html")
+
+@app.route("/oskar")
+def oskar(): 
+    return render_template("oskar.html")
+
+
 
 
 if __name__ == "__main__":
