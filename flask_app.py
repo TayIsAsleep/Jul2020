68127f71802c8ff7cf7fd5187840b10d2c6b063b
dwd03 <drpeperoni@outlook.com>
2020-12-15 12:16:29 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,10 +9,10 @@ def goToPage(linkInput):
         "God Jul till dig Mamma. Tack för att du står ut med mig varje dag, även om jag kan bli jobbig ibland ❤️",
         "God Jul till den bästa pappan som finns! Tack för allt du gör för mig, och för allt vi har gjort genom åren. ❤️",
         "God Jul till bästa storasystern!",
-        "God Jul till Oskar, och tack för att du tar hand om min stora-syster!"
+        "God Jul till Oskar, och tack för att du tar så bra hand om min stora-syster!"
     ]
     allImages = [
-        [],
+        ["https://bit.ly/3r0wuvV","https://bit.ly/3nphEg7","https://bit.ly/3mmh4P1","https://bit.ly/3oVWmau","https://bit.ly/37kq1nN","https://bit.ly/3qXpik3"],
         ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"],
         [],
         []
