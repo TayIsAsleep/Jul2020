f441004e5c468fa5a6e137733ec07bfc1197437f
dwd03 <drpeperoni@outlook.com>
2020-12-16 17:06:33 +0100

    test

--- flask_app.py
+++ flask_app.py
@@ -10,20 +10,15 @@ def goToPage(linkInput):
         "God Jul till den bästa pappan som finns! Tack för allt du gör för mig, och för allt vi har gjort genom åren. ❤️",
         "God Jul till bästa storasystern! Du finns alltid där för mig! Älskar dig!",
         "God Jul till Oskar, och tack för att du tar så bra hand om min stora-syster!",
-        "LINNEA OCH OSKAR"
+        "Ni är bäst!"
     ]
     allImages=[
         ["https://bit.ly/3r0wuvV","https://bit.ly/3nphEg7","https://bit.ly/3mmh4P1","https://bit.ly/3oVWmau","https://bit.ly/37kq1nN","https://bit.ly/3qXpik3"],
         ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"],
         [],
         ["https://bit.ly/2LxBibD"],
-        [f"static/{linkInput}/{x}.png"for x in range(9)]
-        #["static/IMG_0093.png",url_for('static',filename="IMG_0095.png")]
+        [f"static/{linkInput}/{x}.png"for x in range(9)] + ["https://bit.ly/2LxBibD"]
     ]
-
-    
-
-
     allNames=[
         "Mamma",
         "Pappa",
