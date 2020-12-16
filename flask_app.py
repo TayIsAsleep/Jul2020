926ecdd85be3ad73ff226b46f4bf7e01590ce71d
dwd03 <drpeperoni@outlook.com>
2020-12-16 17:16:08 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -17,7 +17,7 @@ def goToPage(linkInput):
         allImages=[
             shuffle(["https://bit.ly/3r0wuvV","https://bit.ly/3nphEg7","https://bit.ly/3mmh4P1","https://bit.ly/3oVWmau","https://bit.ly/37kq1nN","https://bit.ly/3qXpik3"]),
             shuffle(["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"]),
-            shuffle([]),
+            shuffle(["https://bit.ly/2LxBibD"]),
             shuffle(["https://bit.ly/2LxBibD"]),
             shuffle([f"static/{linkInput}/{x}.png"for x in range(9)] + ["https://bit.ly/2LxBibD"])
         ]
