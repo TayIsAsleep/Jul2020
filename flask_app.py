ada3ab43f19437eddef73bd923cc322450486846
dwd03 <drpeperoni@outlook.com>
2020-12-16 19:16:08 +0100

    dawawd

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,6 @@ from flask import Flask,render_template,request,redirect,url_for
 from random import shuffle
 import os
 app = Flask(__name__)
-
 @app.route("/<linkInput>")
 def goToPage(linkInput):
     try:
@@ -11,18 +10,11 @@ def goToPage(linkInput):
         allText=[
             "God Jul till dig Mamma. Tack för att du står ut med mig varje dag, även om jag kan bli jobbig ibland ❤️",
             "God Jul till den bästa pappan som finns! Tack för allt du gör för mig, och för allt vi har gjort genom åren. ❤️",
-            "Ni är bäst!"
+            "Ni är bäst! ❤️"
         ]
-        allNames=[
-            "Mamma",
-            "Pappa",
-            "Linnea och Oskar"
-        ]
-        myImages = [f"static/{linkInput}/{x}" for x in os.listdir(f"Jul2020/static/{linkInput}")]
+        allNames=["Mamma", "Pappa", "Linnea och Oskar"]
+        myImages=[f"static/{linkInput}/{x}" for x in os.listdir(f"Jul2020/static/{linkInput}")]
         shuffle(myImages)
-
-        #url_for('static',filename=linkInput+'.png')
-
         return render_template("index.html", currentName=allNames[selected], titleImage=f"static/{linkInput}.png", images=myImages, personalMessage=allText[selected])
     except Exception as e:
         return render_template("error.html", errormessage=str(e))
