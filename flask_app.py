d05097b1a192f0ecffb489d535906082cb9bb82a
dwd03 <drpeperoni@outlook.com>
2020-12-16 18:42:49 +0100

    awdwda

--- flask_app.py
+++ flask_app.py
@@ -14,6 +14,9 @@ def goToPage(linkInput):
             "God Jul till Oskar, och tack för att du tar så bra hand om min stora-syster!",
             "Ni är bäst!"
         ]
+        
+
+
         allImages=[
             ["https://bit.ly/3r0wuvV","https://bit.ly/3nphEg7","https://bit.ly/3mmh4P1","https://bit.ly/3oVWmau","https://bit.ly/37kq1nN","https://bit.ly/3qXpik3"],
             ["https://bit.ly/34h7pTI","https://bit.ly/3qWRoMa","https://bit.ly/3qZQX3Z","https://bit.ly/37m64gl","https://bit.ly/3oXvLK5","https://bit.ly/2K8Jf6K","https://bit.ly/3oXvMh7"],
@@ -21,6 +24,19 @@ def goToPage(linkInput):
             ["https://bit.ly/2LxBibD"],
             [f"static/{linkInput}/{x}.png"for x in range(9)] + ["https://bit.ly/2LxBibD"]
         ]
+
+
+        import os
+        theDir = os.listdir(f"static/{linkInput}/")
+        
+        raise theDir
+        # for filename in theDir:
+        #     if filename.endswith(".png"):
+        #         # print(os.path.join(directory, filename))
+        #         continue
+        #     else:
+        #         continue
+
         allNames=[
             "Mamma",
             "Pappa",
